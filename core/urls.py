0d45630ff8c6a1d3b366fdc006468720e7540bd3
Syama-57 <syamamohan664@gmail.com>
2026-06-30 11:55:34 +0530

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,11 @@
 """
 URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
 """
 from django.contrib import admin
 from django.urls import path
 from django.http import JsonResponse, HttpResponse
 from bookings.views import movie_list, show_list_by_movie, create_booking
-# 1. Import the JWT views from rest_framework_simplejwt
+
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -27,8 +24,6 @@ urlpatterns = [
     path('api/movies/', movie_list),
     path('api/movies/<int:movie_id>/shows/', show_list_by_movie),
     path('api/bookings/create/', create_booking),
-    
-    # 2. Add these paths so React can authenticate successfully
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
